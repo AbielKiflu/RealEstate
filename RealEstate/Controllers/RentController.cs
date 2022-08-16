8a2d2807f1ccaa813caa92d0025bc99f6cfa0972
Abiel <abiel.kiflu.weld@gmail.com>
2022-08-16 12:04:33 +0200

    Controller rent with index created

---
+++ RealEstate/Controllers/RentController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using RealEstate.Data;
+using RealEstate.Models;
+using System.Diagnostics;
+using RealEstate.ViewModels;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+using Microsoft.AspNetCore.Identity;
+
+namespace RealEstate.Controllers
+{
+    public class RentController : Controller
+    {
+        private readonly ILogger<PropertyController> _logger;
+        private readonly ApplicationDbContext _db;
+        private readonly IHostingEnvironment _env;
+        UserManager<ApplicationUser> _userManager;
+        public static long userID;
+
+
+        public RentController(ApplicationDbContext db, ILogger<PropertyController> logger, IHostingEnvironment env, UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _env = env;
+            _logger = logger;
+            _userManager = userManager;
+
+        }
+
+        
+
+        [HttpGet]
+        public  IActionResult Index()
+        {
+            IEnumerable<Property> rent = _db.Property.Where(p => p.Service == "Rent");
+            return View(rent);
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
